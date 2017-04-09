cfb2930cc616bcd15b2092ba5e583baf223b8aa0
HollyPony <arthur.agombart@gmail.com>
2017-04-09 13:19:12 +0200

    Add basic ES6 support

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: "eslint:recommended",
+  parserOptions: {
+    ecmaVersion: 7,
+    sourceType: "module"
+  }
+  ,
+  env: {
+    "node": true
+  }
+  ,
+  rules: {
+    "no-console": 0,
+    "no-unused-vars": 1
+  }
+};
