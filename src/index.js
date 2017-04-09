c846aa4376076bae38381bf25acf8acdfa952543
HollyPony <arthur.agombart@gmail.com>
2017-04-09 15:38:14 +0200

    Init routing

--- src/index.js
+++ src/index.js
@@ -1,14 +1,31 @@
 import restify from 'restify';
 
-function respond(req, res, next) {
-  res.send('hello ' + req.params.name);
-  next();
-}
-
 let server = restify.createServer();
-server.get('/hello/:name', respond);
-server.head('/hello/:name', respond);
+server.use(restify.CORS());
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+server.get(/.*/, passHere);
+server.post(/.*/, passHere);
+server.put(/.*/, passHere);
+server.del(/.*/, passHere);
 
 server.listen(8080, function() {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+function passHere(req, res, next) {
+  console.log(req.params && Object.keys(req.params).length > 0);
+  res.send(req.params && Object.keys(req.params).length > 0 && parseParams(req.params) || createRandom());
+  next();
+}
+
+function createRandom() {
+  return {id: ""}
+}
+
+function parseParams(params) {
+  return params
+}
+
+export default server;
